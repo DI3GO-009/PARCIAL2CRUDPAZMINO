548759edb7e022f744f96f4c9f5e39093d0db950
Di3G09 <96750103+Latin009x@users.noreply.github.com>
2024-01-20 19:14:11 -0500

    crud2

---
+++ frontend/src/app/views/autores/nuevoautor/nuevoautor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevoautor',
+  standalone: true,
+  imports: [],
+  templateUrl: './nuevoautor.component.html',
+  styleUrl: './nuevoautor.component.css'
+})
+export class NuevoautorComponent {
+
+}
---
+++ frontend/src/app/views/libros/nuevolibro/nuevolibro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevolibro',
+  standalone: true,
+  imports: [],
+  templateUrl: './nuevolibro.component.html',
+  styleUrl: './nuevolibro.component.css'
+})
+export class NuevolibroComponent {
+
+}
